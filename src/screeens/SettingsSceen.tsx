94833be6f0e04cd3c2c030c10a7e2a6807cb0807
EDWINCSN <c.edwin32@hotmail.com>
2026-09-12 12:52:52 -0500

    Paso 1: Configuración Inicial

---
+++ src/screeens/SettingsSceen.tsx
@@ -0,0 +1,19 @@
+import {  Text,StyleSheet} from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+export default function SettingsScreen() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text>pantalla SettingsScreen</Text>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
