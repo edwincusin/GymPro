5e94837a9b127b595f6113f4eacd42f6637f228d
EDWINCSN <c.edwin32@hotmail.com>
2026-09-12 13:52:56 -0500

    Paso 2: Creando las Pestañas (Nivel 2)

--- App.tsx
+++ App.tsx
@@ -1,13 +1,23 @@
 import 'react-native-gesture-handler'
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import TabNavigator from './src/navigators/TabNavigator';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack=createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <StatusBar style='light'/>
+      <Stack.Navigator initialRouteName='TabNavigator'>
+        <Stack.Screen 
+          name='TabNavigator'
+          component={TabNavigator}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ src/navigators/TabNavigator.tsx
@@ -0,0 +1,39 @@
+import { Ionicons } from "@expo/vector-icons";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import ProgressScreen from "../screeens/ProgressScreen";
+import ChestDetailScreen from "../screeens/ChestDetailScreen";
+
+const Tab = createBottomTabNavigator();
+
+export default function TabNavigator() {
+    return (
+        <Tab.Navigator screenOptions={({ route }) => ({
+            headerShown: false,
+            tabBarIcon: ({ focused, color, size }) => {
+                let iconName: any = "list"
+
+                if (route.name === "ProgresoTab") {
+                    iconName = focused ? 'trophy' : 'trophy';
+                }
+                if (route.name === "RutinasTab") {
+                    iconName = focused ? 'clipboard' : 'clipboard-outline';
+                }
+                return <Ionicons name={iconName} size={size} color={color} />
+            },
+            tabBarActiveTintColor:'green',
+            tabBarInactiveTintColor:'gray'
+
+        })}>
+            <Tab.Screen
+                name='ProgresoTab'
+                component={ProgressScreen}
+                options={{ title: 'Progreso' }}
+            />
+            <Tab.Screen
+                name='RutinasTab'
+                component={ChestDetailScreen}
+                options={{ title: 'Rutinas' }}
+            />
+        </Tab.Navigator>
+    );
+}
