26baa88d39a7f7f9f25c66860094cf8da57b39fc
EDWINCSN <c.edwin32@hotmail.com>
2026-09-12 14:32:06 -0500

    Paso 4: El Stack Principal (Nivel 3)

--- App.tsx
+++ App.tsx
@@ -2,20 +2,33 @@ import 'react-native-gesture-handler'
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import TabNavigator from './src/navigators/TabNavigator';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import DrawerNavigator from './src/navigators/DrawerNavigator';
-const Stack=createNativeStackNavigator();
+import ChestDetailScreen from './src/screeens/ChestDetailScreen';
+
+//DICCIONARIO
+export type RootStackParamList={
+  MainDrawer:undefined;
+  ChestDetail:undefined
+}
+
+const Stack=createNativeStackNavigator<RootStackParamList>();
 
 export default function App() {
   return (
     <NavigationContainer>
       <StatusBar style='light'/>
+
       <Stack.Navigator initialRouteName='MainDrawer'>
         <Stack.Screen 
           name='MainDrawer'
           component={DrawerNavigator}
           options={{headerShown:false}}
+        />
+        <Stack.Screen
+          name='ChestDetail'
+          component={ChestDetailScreen}
+          options={{headerShown:true, title:'ChestDetailScreen'}}
         />
       </Stack.Navigator>
     </NavigationContainer>
